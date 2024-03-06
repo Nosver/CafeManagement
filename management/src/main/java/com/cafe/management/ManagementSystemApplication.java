7b21947201508b3fa4ac79d537e60df207eb5fd5
MasisOznigolyan <oznigolyan3@stu.khas.edu.tr>
2024-03-06 16:23:24 +0300

    .

---
+++ management/src/main/java/com/cafe/management/ManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSystemApplication.class, args);
+	}
+
+}
---
+++ management/src/test/java/com/cafe/management/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
