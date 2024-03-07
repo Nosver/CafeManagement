ef1c6cc59f44efd1b703e90f3b76f7e2d69315ea
MasisOznigolyan <oznigolyan3@gmail.com>
2024-03-07 17:39:33 +0300

    updateInventoryItemByName added

--- management/src/main/java/com/cafe/management/repository/InventoryItemRepository.java
+++ management/src/main/java/com/cafe/management/repository/InventoryItemRepository.java
@@ -1,7 +1,20 @@
 package com.cafe.management.repository;
 
-import com.cafe.management.model.InventoryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cafe.management.model.InventoryItem;
+
+import jakarta.transaction.Transactional;
 
 public interface InventoryItemRepository extends JpaRepository<InventoryItem,Long> {
+	
+	@Modifying
+    @Transactional
+    @Query("UPDATE InventoryItem i SET i = :updatedItem WHERE i.itemName = :itemName")
+    int updateItemByName(@Param("itemName") String itemName, @Param("updatedItem") InventoryItem updatedItem);
+
+	public InventoryItem findByItemName(String itemName);
 }
