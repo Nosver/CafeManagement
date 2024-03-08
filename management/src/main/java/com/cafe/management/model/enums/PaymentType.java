3d89d5440394da067d45fc411a737a11e6bd3fa2
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-03-08 16:55:41 +0300

    enum

---
+++ management/src/main/java/com/cafe/management/model/enums/PaymentType.java
@@ -0,0 +1,7 @@
+package com.cafe.management.model.enums;
+
+public enum PaymentType {
+    CASH,
+    CREDIT_CARD,
+    COFFEE_PAY
+}
---
+++ management/src/main/java/com/cafe/management/model/enums/Status.java
@@ -0,0 +1,9 @@
+package com.cafe.management.model.enums;
+
+public enum Status {
+    ORDER_RECEIVED,
+    PREPARING,
+    READY,
+    CANCELLED,
+    SERVED
+}
