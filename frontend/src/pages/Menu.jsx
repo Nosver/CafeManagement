f719d2e1a02ef8c9487ee7261dc0583803f789b6
MasisOznigolyan <oznigolyan3@gmail.com>
2024-03-26 18:36:17 +0300

    menuItem updated

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export const Menu = () => {
     return (<>
-        <MenuItem itemName="Ice americano" imagePath="https://www.thebigsweettooth.com/wp-content/uploads/2020/08/23.-Iced-Americano1.jpg" itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day." itemPrice="90.00"></MenuItem>
+        <MenuItem itemName="Ice americano" imagePath="https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg" itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day." itemPrice="90.00"></MenuItem>
         <MenuItem></MenuItem>
         <MenuItem></MenuItem>
     </>
