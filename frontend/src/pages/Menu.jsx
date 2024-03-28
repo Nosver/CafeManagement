3645d3aea51074f096131ea9405b453cfe311ac6
MasisOznigolyan <oznigolyan3@gmail.com>
2024-03-28 22:38:32 +0300

    pop up added

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,12 +1,54 @@
+import React, { useState } from 'react';
 import MenuItem from "../components/MenuItem";
-
-import React from 'react'
+import Popup from "../components/Popup";
 
 export const Menu = () => {
-    return (<>
-        <MenuItem itemName="Ice americano" imagePath="https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg" itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day." itemPrice="90.00"></MenuItem>
-        <MenuItem></MenuItem>
-        <MenuItem></MenuItem>
-    </>
-    )
-}
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+    const [selectedItemName, setSelectedItemName] = useState("");
+    const [selectedItemDescription, setSelectedItemDescription] = useState("");
+    const [selectedItemPrice, setSelectedItemPrice] = useState("");
+    const [selectedImagePath,setSelectedImagePath] = useState("");
+
+    const openPopup = (name, description, price,path) => {
+        setSelectedItemName(name);
+        setSelectedItemDescription(description);
+        setSelectedItemPrice(price);
+        setIsPopupOpen(true);
+        setSelectedImagePath(path);
+    };
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
+
+    return (
+        <>
+            <MenuItem
+                itemName="Ice americano"
+                imagePath="https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg"
+                itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day."
+                itemPrice="90.00"
+                onClick={() => openPopup("Ice americano", "A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day.", "90.00","https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg")}
+            />
+            <MenuItem
+                itemName="Ice americano"
+                imagePath="https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg"
+                itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day."
+                itemPrice="90.00"
+                onClick={() => openPopup("Ice americano", "A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day.", "90.00","https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg")}
+            />
+            <MenuItem
+                itemName="Ice americano"
+                imagePath="https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg"
+                itemDescription="A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day."
+                itemPrice="90.00"
+                onClick={() => openPopup("Ice americano", "A refreshing blend of bold espresso and chilled water, served over ice. Perfect for a cool pick-me-up on a warm day.", "90.00","https://www.pinoscoffee.com/wp-content/uploads/2022/05/pinos-coffee-ayvalik-iced-americano-150x150.jpg")}
+            />
+            {isPopupOpen && (
+                <Popup onClose={closePopup} itemName={selectedItemName} itemDescription={selectedItemDescription} itemPrice={selectedItemPrice} imagePath={selectedImagePath}/>
+            )}
+        </>
+    );
+};
+
+export default Menu;
