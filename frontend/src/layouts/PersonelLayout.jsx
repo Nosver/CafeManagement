e9c33591e728b3c0bbac007e8d7a1121d34e6459
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2024-03-28 17:04:09 +0300

    Changes on Layouts
    Customer and Personel layouts are added.

---
+++ frontend/src/layouts/PersonelLayout.jsx
@@ -0,0 +1,16 @@
+import { Sidebar } from 'flowbite-react';
+import React, { useState } from 'react'
+import {Outlet} from 'react-router-dom'
+
+
+const PersonelLayout = () => {
+
+
+  return (
+    <>
+      <Outlet/>
+    </>
+  )
+}
+
+export default PersonelLayout
--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomePage from './HomePage'
+import HomePage from './Customer/HomePage'
 import {
     useNavigate,
     Route,
