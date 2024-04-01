493d60a65411af58eb027a4612db9ac8b8ab7b1a
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-04-01 10:30:05 +0300

    sidebarchange

--- frontend/src/pages/Personel/Stocks.jsx
+++ frontend/src/pages/Personel/Stocks.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../../imported-assets/partials/Sidebar';
 import Header from '../../imported-assets/partials/Header';
 import { useState } from 'react';
 import { CButton } from '../../components/CButton';
+import { Siderbar_1 } from '../../components/Siderbar_1';
 
 class stock {
 
@@ -44,97 +45,89 @@ export const Stocks = () => {
 
     return (
         <div>
-            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 
-                <div className="flex h-screen overflow-hidden">
-                    {/* Sidebar */}
-                    <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <Siderbar_1 />
 
-                    {/* Content area */}
-                    <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
-
-
-                        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-
-                        <main>
-                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                    <tr>
-                                        <th scope="col" class="p-4">
+            <div class="p-4 sm:ml-64">
+                <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <main>
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="p-4">
+                                        <div class="flex items-center">
+                                            <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                            <label for="checkbox-all" class="sr-only">checkbox</label>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Stock ID
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Quantity
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Price
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Ongoing Order
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Total Price
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {stocks.map((stock, index) => (
+                                    <tr
+                                        key={index}
+                                        onClick={() => setSelectedOrder(order)}
+                                        class={`
+                                            ${stock.quantity == 1 ? 'bg-red-400' : stock.quantity < 5 ? 'bg-red-300' : stock.quantity < 10 ? 'bg-red-200' : stock.quantity < 20 ? 'bg-red-100' : 'bg-white'} border-b dark:bg-gray-800 dark:border-black-700 hover:bg-white dark:hover:bg-gray-600 }`}
+                                    >
+                                        <td class="w-4 p-4">
                                             <div class="flex items-center">
-                                                <input id="checkbox-all" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                                <label for="checkbox-all" class="sr-only">checkbox</label>
+                                                <input id={`checkbox-table-${index}`} type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                <label for={`checkbox-table-${index}`} class="sr-only">checkbox</label>
                                             </div>
+                                        </td>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {stock.id}
                                         </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Stock ID
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Name
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Quantity
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Price
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Ongoing Order
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Total Price
-                                        </th>
-                                        <th scope="col" class="px-6 py-3">
-                                            Action
-                                        </th>
+                                        <td class="px-6 py-4">
+                                            {stock.name.toString()}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <CButton color='red' name='-' onclick={() => setQuantity(quantity - 1)} />
+                                            {'  ' + stock.quantity.toString()}
+                                            <CButton color='green' name='+' onclick={() => setQuantity(quantity + 1)} />
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {stock.price.toString()}€
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {stock.ongoing_stock.toString()}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            ${stock.total_price.toFixed(2)}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    {stocks.map((stock, index) => (
-                                        <tr
-                                            key={index}
-                                            onClick={() => setSelectedOrder(order)}
-                                            class={`
-                                            ${stock.quantity == 1 ? 'bg-red-400': stock.quantity < 5 ? 'bg-red-300' : stock.quantity < 10 ? 'bg-red-200' : stock.quantity < 20 ? 'bg-red-100' : 'bg-white'} border-b dark:bg-gray-800 dark:border-black-700 hover:bg-white dark:hover:bg-gray-600 }`}
-                                        >
-                                            <td class="w-4 p-4">
-                                                <div class="flex items-center">
-                                                    <input id={`checkbox-table-${index}`} type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                                                    <label for={`checkbox-table-${index}`} class="sr-only">checkbox</label>
-                                                </div>
-                                            </td>
-                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                {stock.id}
-                                            </th>
-                                            <td class="px-6 py-4">
-                                                {stock.name.toString()}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                <CButton color='red' name='-' onclick={() => setQuantity(quantity - 1)}/>
-                                                {'  ' + stock.quantity.toString()}
-                                                <CButton color='green' name='+' onclick={() => setQuantity(quantity + 1)}/>          
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {stock.price.toString()}€
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                {stock.ongoing_stock.toString()}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                ${stock.total_price.toFixed(2)}
-                                            </td>
-                                            <td class="px-6 py-4">
-                                                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </main>
-                    </div>
+                                ))}
+                            </tbody>
+                        </table>
+                    </main>
                 </div>
-
             </div>
+
         </div>
     )
 }
