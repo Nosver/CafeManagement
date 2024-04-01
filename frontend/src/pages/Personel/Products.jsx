928c299024ca55bbfb239a077c8fcadd3cc30347
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-04-01 22:25:02 +0300

    product screen add element

--- frontend/src/pages/Personel/Products.jsx
+++ frontend/src/pages/Personel/Products.jsx
@@ -10,20 +10,18 @@ class product {
         this.category = category;
         this.quantity = quantity;
         this.price = price;
-        this.total_price = price * quantity;
+        this.total_price = total_price;
     }
 
     static getRandomProduct() {
         const id = Math.floor(Math.random() * 1000);
         const names = ['Premium Arabic Coffee', 'Premium Italian Latte', 'Standard Espresso', 'Strawberry Milkshake', 'Caramel Frappuccino', 'Vanilla Ice Cream', 'Black Coffee', 'Green Tea', 'Blueberry Muffin', 'Chocolate Chip Cookie', 'Cinnamon Roll', 'Apple Pie', 'Cheese Cake', 'Brownie', 'Mocha', 'Cappuccino', 'Hot Chocolate', 'Iced Tea', 'Lemonade', 'Orange Juice'];
-        const categories = ['Coffee', 'Tea', 'Bakery', 'Dessert', 'Beverage', 'Snack', 'Pastry', 'Fruit', 'Vegetable', 'Dairy', 'Protein', 'Carb', 'Fat', 'Sugar', 'Salt', 'Spice', 'Herb', 'Sauce', 'Dressing', 'Condiment']
-        
         const randomName = names[Math.floor(Math.random() * names.length)];
         const randomQuantity = Math.floor(Math.random() * 100) + 1;
         const randomPrice = (Math.random() * 20.0).toFixed(2);
         const randomTotalPrice = randomPrice * randomQuantity;
 
-        return new product(id, randomName, randomQuantity, randomPrice, randomTotalPrice);
+        return new product(id, randomName, randomQuantity, randomPrice, randomTotalPrice, 'Other');
     }
 
     static getAllProducts() {
@@ -31,16 +29,17 @@ class product {
         const names = ['Premium Arabic Coffee', 'Premium Italian Latte', 'Standard Espresso', 'Strawberry Milkshake', 'Caramel Frappuccino', 'Vanilla Ice Cream', 'Black Coffee', 'Green Tea', 'Blueberry Muffin', 'Chocolate Chip Cookie', 'Cinnamon Roll', 'Apple Pie', 'Cheese Cake', 'Brownie', 'Mocha', 'Cappuccino', 'Hot Chocolate', 'Iced Tea', 'Lemonade', 'Orange Juice'];
         const categories = ['Coffee', 'Milkshake', 'Frappuccino', 'Ice Cream', 'Tea', 'Muffin', 'Cookie', 'Roll', 'Pie', 'Cake', 'Brownie', 'Mocha', 'Cappuccino', 'Hot Chocolate', 'Iced Tea', 'Lemonade', 'Juice'];
 
-        for(let name of names){
+        for (let name of names) {
             const productName = name;
             const productPrice = (Math.random() * 20.0).toFixed(2);
             const productQuantity = Math.floor(Math.random() * 100) + 1;
             const productIngredient = name; // Assuming the ingredient is the same as the product name
+            const randomID = Math.floor(Math.random() * 1000);
 
             // Assign a category based on the product name
             const productCategory = categories.find(category => productName.includes(category)) || 'Other';
 
-            const newProduct = new product(productName, productIngredient, productQuantity, productPrice, productQuantity, productCategory);
+            const newProduct = new product(randomID, productName, productQuantity, productPrice, productPrice * productQuantity, productCategory);
             products.push(newProduct);
         }
 
@@ -100,6 +99,43 @@ export const Products = () => {
                                     </thead>
 
                                     <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                                        <tr
+
+                                            onClick={() => setSelectedOrder(order)}
+                                            class={`
+                                            'bg-white'} border-b dark:bg-gray-800 dark:border-black-700 hover:bg-white dark:hover:bg-gray-600 }`}
+                                        >
+                                            <td class="w-4 p-4">
+                                                <div class="flex items-center">
+                                                    <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                    <label class="sr-only">checkbox</label>
+                                                </div>
+                                            </td>
+                                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            </th>
+                                            <td class="px-6 py-4">
+                                                <input type="text" class="border rounded-lg px-1 py-1 mt-1 mb-2 w-3/4" placeholder="Enter text" />                                            
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <input type="text" class="border rounded-lg px-1 py-1 mt-1 mb-2 w-3/4" placeholder="Enter text" />
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <input type="text" class="border rounded-lg px-1 py-1 mt-1 mb-2 w-3/4" placeholder="Enter text" />
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <input type="text" class="border rounded-lg px-1 py-1 mt-1 mb-2 w-3/4" placeholder="Enter text" />
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <input type="text" class="border rounded-lg px-1 py-1 mt-1 mb-2 w-3/4" placeholder="$" />
+                                            </td>
+                                            <td class="px-6 py-4">
+                                                <div
+                                                    onClick="..."
+                                                    className="font-medium text-white bg-blue-800 hover:bg-blue-900 dark:bg-blue-700 dark:hover:bg-blue-600 px-4 py-2 rounded cursor-pointer"                                                >
+                                                    Submit
+                                                </div>
+                                            </td>
+                                        </tr>
                                         {products.map((product, index) => (
                                             <tr
                                                 key={index}
@@ -128,7 +164,7 @@ export const Products = () => {
                                                 <td class="px-6 py-4">
                                                     {product.price.toString()}€
                                                 </td>
-                                   
+
                                                 <td class="px-6 py-4">
                                                     ${product.total_price.toFixed(2)}
                                                 </td>
