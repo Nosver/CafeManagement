614cc74a09ab488c364b17d543bcf641639c47a0
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-04-19 17:01:10 +0300

    My Profile In Progress

--- frontend/src/components/personel/ItemPopup.jsx
+++ frontend/src/components/personel/ItemPopup.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Button } from 'flowbite-react';
+import { toast } from 'react-toastify';
 
-export const ItemPopup = ({ title, inputs, closePopup, submitButtonDescription = "Add new item" }) => {
+export const ItemPopup = ({ title, inputs, closePopup, onSubmitFunction, submitButtonDescription = "Add new item" }) => {
 
     const bg_color = "bg-slate-300"
 
@@ -35,18 +36,18 @@ export const ItemPopup = ({ title, inputs, closePopup, submitButtonDescription =
                                     </label>
                                     {input.id === 'img_path' ? (
                                         <div className="flex items-center justify-center w-full">
-                                        <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                            <div className="flex flex-col items-center justify-center pt-2 pb-3">
-                                                <svg className="w-6 h-6 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                                                </svg>
-                                                <p className="mb-1 text-xs text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                                <p className="text-2xs text-gray-500 dark:text-gray-400">SVG, PNG or JPG</p>
-                                            </div>
-                                            <input id="dropzone-file" type="file" className="hidden" accept=".jpg, .jpeg, .png, .gif" />
-                                        </label>
-                                    </div>
-                                    
+                                            <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                                <div className="flex flex-col items-center justify-center pt-2 pb-3">
+                                                    <svg className="w-6 h-6 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                                    </svg>
+                                                    <p className="mb-1 text-xs text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                                    <p className="text-2xs text-gray-500 dark:text-gray-400">SVG, PNG or JPG</p>
+                                                </div>
+                                                <input id="dropzone-file" type="file" className="hidden" accept=".jpg, .jpeg, .png, .gif" />
+                                            </label>
+                                        </div>
+
                                     ) : (
                                         input.placeholder ? (
                                             <input
@@ -72,7 +73,11 @@ export const ItemPopup = ({ title, inputs, closePopup, submitButtonDescription =
                             ))}
 
                         </div>
-                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+                        <button
+                            onClick={() => onSubmitFunction()}
+                            type="submit"
+                            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                        >
                             {submitButtonDescription}
                         </button>
                     </form>
--- frontend/src/pages/Personel/MyProfile.jsx
+++ frontend/src/pages/Personel/MyProfile.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Siderbar_1 } from '../../components/personel/Siderbar_1'
 import { Calendar } from '../../components/partials/Calendar'
 import { Alert } from '../../components/partials/Alert'
 import { toast } from 'react-toastify'
 import faker from 'faker'
+import { ItemPopup } from '../../components/personel/ItemPopup'
+import { ProfleCard } from '../../components/partials/ProfleCard'
+import { ToastToggle } from 'flowbite-react'
+
+
 /*
   Employee ifo:
   - Name
@@ -13,12 +18,15 @@ import faker from 'faker'
 */
 
 class Employee {
-  constructor(name, surname, email, password, address, phone, role, department, salary, date_of_birth, date_of_employment, date_of_dismissal, is_active, is_deleted, created_at, updated_at) {
+
+  constructor(name, surname, email, password, address, role, photo) {
     this.name = name
     this.surname = surname
     this.email = email
     this.password = password
     this.address = address
+    this.role = role
+    this.photo = photo
   }
 
   createRandomEmployee() {
@@ -26,7 +34,9 @@ class Employee {
     this.surname = faker.name.lastName()
     this.email = faker.internet.email()
     this.password = faker.internet.password()
-   this.address = `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.state()} ${faker.address.zipCode()}`;
+    this.address = `${faker.address.streetAddress()}, ${faker.address.city()}, ${faker.address.state()} ${faker.address.zipCode()}`;
+    this.role = faker.name.jobTitle()
+    this.photo = "https://assets-us-01.kc-usercontent.com/b1495851-c47d-0087-29c8-65e1780b1b3b/686e81a5-0a9e-4bfb-9812-c23ac8fca54f/Shutterstock_1296008485%20square.jpg"
   }
 }
 
@@ -36,6 +46,8 @@ export const MyProfile = () => {
   const employee = new Employee()
   employee.createRandomEmployee()
 
+  const [isPasswordPopupVisible, setisPasswordPopupVisible] = useState(false)
+
   return (
     <div>
 
@@ -44,50 +56,98 @@ export const MyProfile = () => {
       <div class="p-4 sm:ml-64">
         <div class="p-4 border-2 border-slate-500 border-dashed rounded-lg dark:border-gray-700">
           <h1 class="mb-4 text-2xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-4xl dark:text-white">
-          {'Welcome back, ' + employee.name + ' 👋'}
+            {'Welcome back, ' + employee.name + ' 👋'}
           </h1>
           <div className="border-t border-gray-200 my-4 dark:border-gray-700"></div>
 
+          <div className='flex flex-row ml-10'>
 
-          <form>
+            <div className='flex  mt-10'>
 
-            <div class="mb-6">
-              <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-              <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={employee.name + ' ' + employee.surname} required
-                readOnly />
+              <div className='min-w-80'>
+                <ProfleCard Person={employee} />
+              </div>
+
             </div>
 
-            <div class="mb-6">
-              <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
-              <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={employee.address} required
-                />
-            </div>
-
-            <div class="mb-6">
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-              <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder={employee.email} required
-                readOnly />
-            </div>
-
-            <div class="mb-6">
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-              <input
-                type="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder={employee.password.replace(/.(?=.{2})/g, '*')}
-                required
-                readOnly
+            <div>
+              {isPasswordPopupVisible && <ItemPopup
+                title='Change Password'
+                onClose={() => setisPasswordPopupVisible(false)}
+                inputs={[
+                  {
+                    label: 'Old Password',
+                    type: 'password',
+                    placeholder: '',
+                    required: true
+                  },
+                  {
+                    label: 'New Password',
+                    type: 'password',
+                    placeholder: '',
+                    required: true
+                  },
+                  {
+                    label: 'Confirm New Password',
+                    type: 'password',
+                    placeholder: '',
+                    required: true
+                  }
+                ]}
+                onSubmitFunction={() => toast.success('Password changed successfully')}
+                submitButtonDescription='Change Password'
+                closePopup={() => setisPasswordPopupVisible(false)}
               />
+              }
+
             </div>
 
-            <button type="submit" class="w-24 py-2.5 bg-blue-700 text-white text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-600">Update</button>
+            <form
+              className='min-w-fit w-1/2 mx-auto'
+            >
 
-          </form>
+              <div class="mb-6">
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={employee.name + ' ' + employee.surname} required
+                  readOnly />
+              </div>
 
+              <div class="mb-6">
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Address</label>
+                <textarea type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  value={employee.address} required
+                />
+              </div>
+
+              <div class="mb-6">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder={employee.email} required
+                  readOnly />
+              </div>
+
+              <div class="mb-6">
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input
+                  type="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder={employee.password.replace(/.(?=.{2})/g, '*')}
+                  required
+                  readOnly
+                />
+              </div>
+
+              <button type="submit" class="w-24 py-2.5 bg-blue-700 text-white text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 hover:bg-blue-600">Update</button>
+
+              <button
+                onClick={() => setisPasswordPopupVisible(true)}
+                type='button' class="w-fit ml-5 py-2.5 bg-orange-700 text-white text-sm rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 hover:bg-red-600">
+                Change Password
+              </button>
+            </form>
+          </div>
 
           <div className="border-t border-gray-200 my-4 dark:border-gray-700"></div>
           <Calendar />
