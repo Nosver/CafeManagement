ac1b7bb86efd912bf6e154d11442f0f58645d376
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-04-20 17:30:28 +0300

    password popup added

--- frontend/src/components/partials/PasswordPopup.jsx
+++ frontend/src/components/partials/PasswordPopup.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import 'flowbite';
-import { initFlowbite } from 'flowbite'
+import { toast } from 'react-toastify';
 
 export const PasswordPopup = ({ closePopup }) => {
 
     const [password, setPassword] = useState('');
+    const [passwordAgain, setPasswordAgain] = useState('');
     const [isLongEnough, setIsLongEnough] = useState(false);
     const [hasLowercase, setHasLowercase] = useState(false);
     const [hasSpecialChar, setHasSpecialChar] = useState(false);
+    const [passwordMatch, setPasswordMatch] = useState(false);
 
     useEffect(() => {
         setIsLongEnough(password.length >= 10);
@@ -20,6 +22,33 @@ export const PasswordPopup = ({ closePopup }) => {
         setPassword(event.target.value);
     };
 
+    useEffect(() => {
+        if (password != null && passwordAgain != null && passwordAgain.length > 0) {
+            setPasswordMatch(password === passwordAgain);
+        }
+    }, [password, passwordAgain]);
+
+    const handlePasswordMatch = (event) => {
+        setPasswordMatch(event.target.value === password);
+    }
+
+   const handleSubmit = (event) => {
+    if(passwordAgain && isLongEnough && hasLowercase && hasSpecialChar && passwordMatch){
+        console.log("Password changed successfully");
+        toast.success("Password changed successfully");
+    }
+    else{
+        console.log("Password change failed");
+        toast.error("Password doesn't meet the requirements");
+
+        setPassword('');
+        setPasswordAgain('');
+        setIsLongEnough(false);
+        setHasLowercase(false);
+        setHasSpecialChar(false);
+        setPasswordMatch(false);
+    }
+}
 
     return (
 
@@ -70,12 +99,35 @@ export const PasswordPopup = ({ closePopup }) => {
                                     </svg>
                                     At least one special character, e.g., ! @ # ?
                                 </li>
+
+                                <li class="flex items-center">
+                                    <svg
+                                        className={`w-3.5 h-3.5 me-2 ${passwordMatch ? 'text-green-500' : 'text-gray-500'} dark:text-gray-400 flex-shrink-0`}
+                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    New Passwords should match
+                                </li>
                             </ul>
+                            <div></div>
 
 
                             <form>
+
                                 <div class="mb-6">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Password</label>
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-4">Old Password</label>
+                                    <input data-popover-target="popover-password" data-popover-placement="bottom" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    <div data-popover id="popover-password" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
+                                        <div class="p-3 space-y-2">
+                                            <h3 class="font-semibold text-gray-900 dark:text-white">Must have at least 6 characters</h3>
+
+                                        </div>
+                                        <div data-popper-arrow></div>
+                                    </div>
+                                </div>
+
+                                <div class="mb-6">
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
                                     <input
                                         data-popover-target="popover-password"
                                         data-popover-placement="bottom"
@@ -85,29 +137,38 @@ export const PasswordPopup = ({ closePopup }) => {
                                         required
                                         value={password}
                                         onChange={handlePasswordChange}
-                                    />                                <div data-popover id="popover-password" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
-                                        
-                                        <div data-popper-arrow></div>
-                                    </div>
-                                </div>
-                                <div class="mb-6">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Password</label>
-                                    <input data-popover-target="popover-password" data-popover-placement="bottom" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                    />
+
                                     <div data-popover id="popover-password" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
-                                        <div class="p-3 space-y-2">
-                                            <h3 class="font-semibold text-gray-900 dark:text-white">Must have at least 6 characters</h3>
-                                            
-                                        </div>
+
                                         <div data-popper-arrow></div>
                                     </div>
                                 </div>
-                                <div class="flex items-start mb-6">
-                                    <div class="flex items-center h-5">
-                                        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                                    </div>
-                                    <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+
+                                <div class="mb-6">
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password Again</label>
+                                    <input
+                                        data-popover-target="popover-password"
+                                        data-popover-placement="bottom"
+                                        type="password"
+                                        id="password"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                        value={passwordAgain}
+                                        onChange={(e) => {
+                                            setPasswordAgain(e.target.value);
+                                            handlePasswordMatch(password, e.target.value);
+                                        }}
+                                    />
+
                                 </div>
-                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+
+
+                                <button 
+                                onClick={handleSubmit}
+                                type="submit" class="text-white bg-pink-800 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    Change Password
+                                </button>
                             </form>
 
                         </div>
