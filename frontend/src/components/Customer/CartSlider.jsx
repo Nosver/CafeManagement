f592625f0b82019e4a4d11b9f67bdd5c1c463ced
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-04-20 16:42:07 +0300

    Cart bg bug fix

--- frontend/src/components/Customer/CartSlider.jsx
+++ frontend/src/components/Customer/CartSlider.jsx
@@ -82,7 +82,7 @@ export const CartSlider = () => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-hidden">
--- frontend/src/components/partials/PasswordPopup.jsx
+++ frontend/src/components/partials/PasswordPopup.jsx
@@ -75,7 +75,7 @@ export const PasswordPopup = ({ closePopup }) => {
 
                             <form>
                                 <div class="mb-6">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Password</label>
                                     <input
                                         data-popover-target="popover-password"
                                         data-popover-placement="bottom"
@@ -86,72 +86,17 @@ export const PasswordPopup = ({ closePopup }) => {
                                         value={password}
                                         onChange={handlePasswordChange}
                                     />                                <div data-popover id="popover-password" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
-                                        <div class="p-3 space-y-2">
-                                            <h3 class="font-semibold text-gray-900 dark:text-white">Must have at least 6 characters</h3>
-                                            <div class="grid grid-cols-4 gap-2">
-                                                <div class="h-1 bg-orange-300 dark:bg-orange-400"></div>
-                                                <div class="h-1 bg-orange-300 dark:bg-orange-400"></div>
-                                                <div class="h-1 bg-gray-200 dark:bg-gray-600"></div>
-                                                <div class="h-1 bg-gray-200 dark:bg-gray-600"></div>
-                                            </div>
-                                            <p>It’s better to have:</p>
-                                            <ul>
-                                                <li class="flex items-center mb-1">
-                                                    <svg class="w-3.5 h-3.5 me-2 text-green-400 dark:text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                                                    </svg>
-                                                    Upper & lower case letters
-                                                </li>
-                                                <li class="flex items-center mb-1">
-                                                    <svg class="w-3 h-3 me-2.5 text-gray-300 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                    </svg>
-                                                    A symbol (#$&)
-                                                </li>
-                                                <li class="flex items-center">
-                                                    <svg class="w-3 h-3 me-2.5 text-gray-300 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                    </svg>
-                                                    A longer password (min. 12 chars.)
-                                                </li>
-                                            </ul>
-                                        </div>
+                                        
                                         <div data-popper-arrow></div>
                                     </div>
                                 </div>
                                 <div class="mb-6">
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Password</label>
                                     <input data-popover-target="popover-password" data-popover-placement="bottom" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                     <div data-popover id="popover-password" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
                                         <div class="p-3 space-y-2">
                                             <h3 class="font-semibold text-gray-900 dark:text-white">Must have at least 6 characters</h3>
-                                            <div class="grid grid-cols-4 gap-2">
-                                                <div class="h-1 bg-orange-300 dark:bg-orange-400"></div>
-                                                <div class="h-1 bg-orange-300 dark:bg-orange-400"></div>
-                                                <div class="h-1 bg-gray-200 dark:bg-gray-600"></div>
-                                                <div class="h-1 bg-gray-200 dark:bg-gray-600"></div>
-                                            </div>
-                                            <p>It’s better to have:</p>
-                                            <ul>
-                                                <li class="flex items-center mb-1">
-                                                    <svg class="w-3.5 h-3.5 me-2 text-green-400 dark:text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
-                                                    </svg>
-                                                    Upper & lower case letters
-                                                </li>
-                                                <li class="flex items-center mb-1">
-                                                    <svg class="w-3 h-3 me-2.5 text-gray-300 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                    </svg>
-                                                    A symbol (#$&)
-                                                </li>
-                                                <li class="flex items-center">
-                                                    <svg class="w-3 h-3 me-2.5 text-gray-300 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                                    </svg>
-                                                    A longer password (min. 12 chars.)
-                                                </li>
-                                            </ul>
+                                            
                                         </div>
                                         <div data-popper-arrow></div>
                                     </div>
