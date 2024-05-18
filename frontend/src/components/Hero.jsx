0a82b44ea43a29b28b3840f95d5bf9f03a05b999
Doğukan <86402638+Dogukan0101@users.noreply.github.com>
2024-05-18 21:50:37 +0300

    Update Hero.jsx
    .

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,13 +7,13 @@ const Hero = ({
     return (
       <section className='bg-custom-coffe-brown py-20 flex flex-row'>
 
-        <div className='max-w-7xl ml-40 mr-10 px-4 sm:px-6 lg:px-8 flex flex-col'>
+        <div className='w-8/12 flex flex-col'>
           <div className='text-center flex flex-col h-full justify-center'>
             <h1 className='text-5xl font-extrabold text-white sm:text-5xl md:text-6xl'>{title}</h1>
             <p className='my-4 text-xl text-white'>{subtitle}</p>
           </div>
         </div>
-        <div className='max-w-7xl mx-5 px-4 sm:px-6 lg:px-8 flex justify-center'>
+        <div className='flex flex-col justify-start w-4/12'>
             <img className='rounded-full mb-4 mx-2' src={image} width={220}></img>
         </div>
 
