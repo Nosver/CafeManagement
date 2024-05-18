5b803eb2050bf96a1ab8933b5686871e88c19c05
Kml159 <99826221+Kml159@users.noreply.github.com>
2024-05-18 21:44:10 +0300

    Customer access prevented to the ERP side

--- frontend/src/layouts/PersonelLayout.jsx
+++ frontend/src/layouts/PersonelLayout.jsx
@@ -1,13 +1,24 @@
 import { Sidebar } from 'flowbite-react';
 import React, { useState } from 'react'
 import {Outlet} from 'react-router-dom'
-
+import Cookies from 'js-cookie';
 
 const PersonelLayout = () => {
+
+  const ROLE = Cookies.get('role');
+
+  if(ROLE !== "ADMIN" && ROLE !== "EMPLOYEE"){
+    return (
+      <div>
+        <UnauthorizedPage />
+      </div>
+    );
+  }
 
   return (
     <>
       <Outlet/>
+
     </>
   )
 }
